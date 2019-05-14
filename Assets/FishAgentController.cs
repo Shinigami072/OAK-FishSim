5ac96674d02a01e9d9ba454a3e5428355f08a7dc
Shinigami072 <krzys.stasiowski@gmail.com>
2019-05-14 15:08:43 +0200

    this Works mostly

--- Assets/FishAgentController.cs
+++ Assets/FishAgentController.cs
@@ -10,71 +10,74 @@ namespace DefaultNamespace
         private List<FishMovementStrategy> _strategies;
         private Vector3 _directionSum;
         private float _weightSum;
-        private float _velocitySum;
+        private Vector3 _velocitySum;
 
-        
+
         public Vector3 direction = Vector3.forward;
         public float veloctity = 0.0f;
+        private Rigidbody _rigidbody;
 
 
-       
         private void Start()
         {
+            _rigidbody = GetComponent<Rigidbody>();
             List<Component> strategiesComponents = new List<Component>();
-            GetComponents(typeof(FishMovementStrategy),strategiesComponents);
+            GetComponents(typeof(FishMovementStrategy), strategiesComponents);
             _strategies = new List<FishMovementStrategy>(strategiesComponents.Count);
-            
+
             foreach (var strategy in strategiesComponents)
             {
                 _strategies.Add(strategy as FishMovementStrategy);
             }
 
             direction = Random.onUnitSphere;
-            veloctity = Random.Range(0f, 0.1f);
-
-
+            veloctity = Random.Range(0.1f, 0.5f);
         }
 
         private void Update()
         {
-            transform.rotation = Quaternion.LookRotation(direction,Vector3.up);
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
         }
 
         private void OnDrawGizmos()
 
         {
-            var position = transform.position;    
-            Debug.DrawLine(position + Vector3.up, position + Vector3.up + direction*veloctity * 1000, Color.yellow);
+            var position = transform.position;
+//            Debug.DrawLine(position + Vector3.up, position + Vector3.up + direction * veloctity * 1000, Color.yellow);
+            Debug.DrawLine(position, position + direction * 500, Color.magenta);
         }
-        
+
         private void FixedUpdate()
         {
             _directionSum = Vector3.zero;
-            _velocitySum = 0.0f;
+            _velocitySum = Vector3.zero;
             _weightSum = 0f;
-           
+
             foreach (var strategy in _strategies)
             {
-                _directionSum += strategy.GetDirection(direction, veloctity) * strategy.weight;
+//                _directionSum += strategy.GetDirection(direction, veloctity) * strategy.weight;
                 _velocitySum += strategy.GetVelocity(direction, veloctity) * strategy.weight;
                 _weightSum += strategy.weight;
-
             }
-            var wantedDirection = _directionSum.normalized;
-            
-            //lerp - funkcja przekształcająca wektor A do B, zgodnei ze skalą c,
-            //jeśli c = 0 zwraca A
-            //jeśli c = 1 zwraca B
-            //użwyamy delty żeby zmiana była płynniejsza
-            direction = Vector3.Lerp(direction.normalized,wantedDirection.normalized,Time.fixedTime/15.0f);
+//
+            var wantedDirection = _velocitySum/_weightSum;
+//
+//            var diff = (wantedDirection.normalized - direction) * Time.fixedTime;
+//            //lerp - funkcja przekształcająca wektor A do B, zgodnei ze skalą c,
+//            //jeśli c = 0 zwraca A
+//            //jeśli c = 1 zwraca B
+//            //użwyamy delty żeby zmiana była płynniejsza
+//
+//            direction += diff* Time.fixedTime;
+//            direction.Normalize();
 
-            
+//            veloctity = Mathf.Lerp(veloctity, wantedDirection.magnitude, Time.fixedTime / 15.0f);
 //            veloctity *= 0.9f;
-            veloctity = Mathf.Lerp(veloctity, _velocitySum / _weightSum, Time.fixedTime/15.0f);
             //_directionSum.magnitude / _weightSum;
-
-
-            transform.position += veloctity * direction * Time.fixedTime;
+            direction = wantedDirection.normalized;
+            veloctity = wantedDirection.magnitude;
+//            _rigidbody.position+= direction*veloctity*Time.fixedTime;
+            transform.position += wantedDirection*Time.fixedTime;
         }
     }
 }
--- Assets/FlockMovementStrategy.cs
+++ Assets/FlockMovementStrategy.cs
@@ -12,47 +12,46 @@ namespace DefaultNamespace
         public float maxVelocity = 0.5f;
 
         private Vector3 _sumDirection = Vector3.zero;
-        private float _sumSpeed = 0;
-        private int _fishCount = 0;
+        private Vector3 _sumSpeed = Vector3.zero;
+        private float _fishCount = 0;
         private Vector3 _outDir = Vector3.zero;
         private Vector3 _sumDirectionCalc;
-        private float _sumSpeedCalc;
-        private int _fishCountCalc;
+        private Vector3 _sumSpeedCalc;
+        private float _fishCountCalc;
+        private FishAgentController controller;
 
         private void Start()
         {
+            controller = GetComponent<FishAgentController>();
             //range check
             if (repulsionRange > alignmentRange || alignmentRange > attractionRange || repulsionRange > attractionRange)
                 throw new ArgumentOutOfRangeException();
-            if(maxVelocity<=0)
-                throw new ArgumentOutOfRangeException("maxVelocity","it should be >0");
-            
+            if (maxVelocity <= 0)
+                throw new ArgumentOutOfRangeException("maxVelocity", "it should be >0");
         }
 
         private void FixedUpdate()
         {
             //transfer calculated values
-            _sumDirectionCalc = _sumDirection;
+//            _sumDirectionCalc = _sumDirection;
             _sumSpeedCalc = _sumSpeed;
             _fishCountCalc = _fishCount;
-            
+
             //resetAccumulators
-            _sumSpeed = 0f;
-            _sumDirection = Vector3.zero;
+            _sumSpeed = Vector3.zero;
+//            _sumDirection = Vector3.zero;
             _fishCount = 0;
         }
 
-        public override Vector3 GetDirection(Vector3 direction, float velocity)
-        {
-            _outDir =  (_sumDirectionCalc + direction) / (_fishCountCalc + 1);
+//        public override Vector3 GetDirection(Vector3 direction, float velocity)
+//        {
+//            _outDir = ((_sumSpeedCalc + direction * velocity) / (_fishCountCalc + 1));
+//            return ((_sumSpeedCalc + direction * velocity) / (_fishCountCalc + 1)).normalized;
+//        }
 
-            return (_sumDirectionCalc + direction) / (_fishCountCalc + 1);
-        }
-
-        public override float GetVelocity(Vector3 direction, float velocity)
+        public override Vector3 GetVelocity(Vector3 direction, float velocity)
         {
-            return (_sumSpeedCalc + velocity) /
-                 (_fishCountCalc + 1);
+            return ((_sumSpeedCalc + direction * velocity) / (_fishCountCalc + 1));
         }
 
         //Debug Drawing
@@ -77,38 +76,44 @@ namespace DefaultNamespace
         {
             if (other.CompareTag("FishAgent"))
             {
-                
                 FishAgentController otherFish = other.gameObject.GetComponent<FishAgentController>();
-                
+
                 var position = transform.position;
                 var otherPosition = other.transform.position;
 
                 var dist = Vector3.Distance(position, otherPosition);
 
+//                var dir = 0.1f * Vector3.Dot(controller.direction, (otherPosition - position).normalized);
+//                if (dir < 0.05f)
+//                    dir = 0;
+//                dir += 1.0f;
+                var dir = 1.0f;
                 //not collide
                 if (dist < repulsionRange)
                 {
-                    _fishCount++;
-                    _sumDirection += -(otherPosition - position).normalized;
-                    _sumSpeed += maxVelocity * (1 - dist / repulsionRange);
+                    _fishCount += dir;
+                    var d = -(otherPosition - position).normalized * dir;
+//                    _sumDirection += d;
+                    _sumSpeed += d * maxVelocity * (1 - dist / repulsionRange);
                 }
-                
+
                 //align movement
                 else if (dist < alignmentRange)
                 {
-                    _fishCount++;
-                    _sumDirection += otherFish.direction;
-                    _sumSpeed += otherFish.veloctity;
+                    _fishCount += dir;
+                    var d = otherFish.direction * dir;
+//                    _sumDirection += d;
+                    _sumSpeed += d * otherFish.veloctity;
                 }
-                
+
                 //move together
                 else if (dist < attractionRange)
                 {
-                    _fishCount++;
-                    _sumDirection += (otherPosition - position).normalized;
-                    _sumSpeed += maxVelocity * ((dist-alignmentRange) / (attractionRange-alignmentRange));
+                    _fishCount += dir;
+                    var d = (otherPosition - position).normalized * dir;
+//                    _sumDirection += d;
+                    _sumSpeed += d * maxVelocity * (dist - alignmentRange) / (attractionRange - alignmentRange);
                 }
-
             }
         }
     }
