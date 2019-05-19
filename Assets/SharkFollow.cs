56f5b6f4c6189a945657d40a144d72af39633fa7
AleksandraKasznia <33521222+AleksandraKasznia@users.noreply.github.com>
2019-05-19 14:00:38 +0200

    Shark follows an object

---
+++ Assets/SharkFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkFollow : MonoBehaviour
+{
+    public Transform aTarget;
+
+    private float mSpeed = 10.0f;
+
+    private const float EPSILON = 0.1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        transform.LookAt(aTarget.position);
+            if((transform.position - aTarget.position).magnitude > EPSILON)
+                transform.Translate(0.0f,0.0f, mSpeed * Time.deltaTime);
+
+    }
+}
