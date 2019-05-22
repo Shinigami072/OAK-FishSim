557aaf47dd6a430593f065714a21adeca9e79953
AleksandraKasznia <33521222+AleksandraKasznia@users.noreply.github.com>
2019-05-22 18:49:29 +0200

    Shark follows the nearest object inside its sight

---
+++ Assets/SharkCloseObjects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkCloseObjects : MonoBehaviour
+{
+    private List<Transform> objects = new List<Transform>();
+
+    public List<Transform> GetObjects()
+    {
+        return objects;}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!objects.Contains(other.gameObject.transform))
+        {
+            objects.Add(other.gameObject.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objects.Remove(other.gameObject.transform);
+    }
+}
